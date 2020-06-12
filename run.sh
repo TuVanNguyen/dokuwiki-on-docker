7c51d109f63d9066e84dc54bb121bcb283506669
Tu Van Nguyen <tuvannguyen33@gmail.com>
2020-06-12 11:04:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+BASEDIR=$(dirname $0) #go to directory where script is located
+cd $BASEDIR
 sudo git pull
 sleep 1 && xdg-open http://localhost:80 &
 sudo docker-compose up --remove-orphans 
